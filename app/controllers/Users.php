ce6b60953aa86ef751a8646d372a25ed18228647
Nazanin Saeidi <71193970+nazanin-saeidi@users.noreply.github.com>
2021-04-04 16:22:47 +0430

    Add files via upload

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -5,8 +5,62 @@
         }
         public function register() {
             //check if it's a POST request
-            if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            if($_SERVER['REQUEST_METHOD'] == 'POST') {
+                
                 //Process the form
+                $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+                
+                $data = [
+                    'first_name' => trim($_POST['first_name']),
+                    'last_name' => trim($_POST['last_name']),
+                    'email' => trim($_POST['email']),
+                    'password' => trim($_POST['password']),
+                    'confirm_password' => trim($_POST['confirm_password']),
+                    'first_name_error' => '',
+                    'last_name_error' => '',
+                    'email_error' => '',
+                    'password_error' => '',
+                    'confirm_password_error' => '',
+                ];
+
+                //Validate first name
+                if(empty($data['first_name'])) {
+                    $data['first_name_error'] = 'Please enter first name!';
+                }
+
+                //Validate last name
+                if(empty($data['last_name'])) {
+                    $data['last_name_error'] = 'Please enter last name!';
+                }
+
+                //Validate email
+                if(empty($data['email'])) {
+                    $data['email_error'] = 'Please enter email!';
+                }
+
+                //Validate password
+                if(empty($data['password'])) {
+                    $data['password_error'] = 'Please enter password!';
+                } elseif (strlen($data['password']) < 6) {
+                    $data['password_error'] = 'Password must be at least 6 characters!';
+                }
+
+                //Validate confirm password
+                if(empty($data['confirm_password'])) {
+                    $data['confirm_password_error'] = 'Please confirm the password!';
+                } else {
+                    if($data['password'] != $data['confirm_password']) {
+                        $data['confirm_password_error'] = 'Passwords do not match!';
+                    }
+                }
+
+                
+                if(empty($data['first_name_error']) && empty($data['last_name_error']) && 
+                empty($data['email_error']) && empty($data['password_error']) && empty($data['confirm_password_error'])) {
+                    die('SUCCESS');
+                } else {
+                    $this->view('users/register', $data);
+                }
 
             } else {
                 //Load the register form
@@ -33,6 +87,12 @@
 
             } else {
                 //Load the login form
+                $data = [
+                    'email' => '',
+                    'password' => '',
+                    'email_error' => '',
+                    'password_error' => ''
+                ];
                 return $this->view('users/login');
             }
         }
