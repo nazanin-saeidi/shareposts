a46d2fce6688f0614f71aa4b31694352dbc31334
Nazanin Saeidi <71193970+nazanin-saeidi@users.noreply.github.com>
2021-04-04 16:30:20 +0430

    Add files via upload

--- app/views/users/login.php
+++ app/views/users/login.php
@@ -1,19 +1,34 @@
 <?php require APP_ROOT . '/views/includes/header.php'; ?>
-    <h3>Login Form</h3>
-    <form>
-        <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    <div class="row">
+      <div class="col-md-6 mx-auto">
+        <div class="card card-body bg-light mt-5">
+          <h3>Login</h3>
+          <p>Please fill in youe credentials to log in</p>
+          <form action="<?php echo URL_ROOT . '/users/login'; ?>" method="POST">
+              <div class="form-group">
+                <label for="email">Email: <sup>*</sup></label>
+                <input type="email" name="email" class="form-control form-control-lg <?php echo (!empty($data
+                ['email_error'])) ? 'is-invalid' : ''; ?>" id="email" value="<?php echo isset($data['email'])
+                ? $data['email'] : ''; ?>">
+                <span class="invalid-feedback"><?php echo $data['email_error']; ?></span>
+              </div>
+              <div class="form-group">
+                <label for="password">Password: <sup>*</sup></label>
+                <input type="password" name="password" class="form-control form-control-lg <?php echo (!empty($data
+                ['password_error'])) ? 'is-invalid' : ''; ?>" id="password" value="<?php echo isset($data['password'])
+                ? $data['password'] : ''; ?>">
+                <span class="invalid-feedback"><?php echo $data['password_error']; ?></span>
+              </div>
+              <div class="row mt-3">
+                <div class="col">
+                  <input value="Login" type="submit" class="btn btn-success btn-block" style="width: 100%;" />
+                </div>
+                <div class="col">
+                  <a href="<?php echo URL_ROOT . '/users/register'; ?>" class="btn btn-light btn-block">No account? Register</a>
+                </div>
+              </div>             
+          </form>
         </div>
-        <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-        </div>
-        <div class="form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+      </div>
+    </div>
 <?php require APP_ROOT . '/views/includes/footer.php'; ?>
